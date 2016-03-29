f140c38a43d1c9b0d96e46f6ffab05010210d2ca
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2016-03-29 18:56:37 +0200

    Lookup ancestors for nearest aspect. TODO: lookup and stack all.

--- lib/kungfuig/jobber.rb
+++ lib/kungfuig/jobber.rb
@@ -23,6 +23,10 @@ module Kungfuig
         )
       end
 
+      # rubocop:disable Metrics/AbcSize
+      # rubocop:disable Metrics/CyclomaticComplexity
+      # rubocop:disable Metrics/PerceivedComplexity
+      # rubocop:disable Metrics/MethodLength
       def bottleneck(receiver, method, result, *args)
         respond_to = ->(m, r) { r.respond_to? m.to_sym }
         r = case receiver
@@ -31,14 +35,23 @@ module Kungfuig
             when respond_to.curry[:to_h] then receiver.to_h
             else receiver
             end
-        receiver_class = receiver.class.ancestors.detect do |c|
+        return unless (receiver_class = receiver.class.ancestors.detect do |c|
           @hash[c.name] && @hash[c.name][method]
+        end)
+
+        job = Kernel.const_get(@hash[receiver_class.name][method])
+        if Kernel.const_defined?('Rails') && Rails.env.development?
+          job.new.perform(r, method, result, *args)
+        else
+          job.perform_async(r, method, result, *args)
         end
-        Kernel.const_get(@hash[receiver_class.name][method])
-              .perform_async(r, method, result, *args) if receiver_class
       rescue => e
         Kungfuig.✍("Fail [#{e.message}] while #{receiver}", method, result, *args)
       end
+      # rubocop:enable Metrics/MethodLength
+      # rubocop:enable Metrics/PerceivedComplexity
+      # rubocop:enable Metrics/CyclomaticComplexity
+      # rubocop:enable Metrics/AbcSize
     end
   end
 end
